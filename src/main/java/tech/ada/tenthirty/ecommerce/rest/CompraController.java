13664fbf7f8c568ef58f77580dd64091a9de5671
Matheus Sousa <alvessousam6@gmail.com>
2026-04-28 21:38:52 -0300

    Correção do projeto e adição de testes unitários

--- src/main/java/tech/ada/tenthirty/ecommerce/rest/CompraController.java
+++ src/main/java/tech/ada/tenthirty/ecommerce/rest/CompraController.java
@@ -4,7 +4,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 import tech.ada.tenthirty.ecommerce.payload.CompraRequest;
 import tech.ada.tenthirty.ecommerce.payload.ItemAdicionadoRequest;
 import tech.ada.tenthirty.ecommerce.payload.response.CompraResponse;
+import tech.ada.tenthirty.ecommerce.queue.ReservarItemEstoqueProducer;
+import tech.ada.tenthirty.ecommerce.queue.payload.ReservarEstoqueRequest;
 import tech.ada.tenthirty.ecommerce.services.AdicionarProdutosService;
 import tech.ada.tenthirty.ecommerce.services.RealizarCompraService;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping(name = "/compra")
 @RequiredArgsConstructor
+@Slf4j
 public class CompraController {
 
     private final RealizarCompraService realizarCompraService;
     private final AdicionarProdutosService adicionarProdutosService;
+    private final ReservarItemEstoqueProducer reservarItemEstoqueProducer;
     @Operation(summary = "Realizar a compra")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "compra realizada com sucesso"),
@@ -45,6 +55,15 @@ public class CompraController {
     @ResponseStatus(HttpStatus.CREATED)
     public CompraResponse adicionarItem(@RequestBody ItemAdicionadoRequest itemAdicionadoRequest){
         return adicionarProdutosService.execute(itemAdicionadoRequest);
+    }
+
+    @GetMapping(value = "/{notificacao}")
+    public void enviaNotificacao(@PathVariable String notificacao){
+        ReservarEstoqueRequest reservarEstoqueRequest = new ReservarEstoqueRequest();
+        reservarEstoqueRequest.setDataCompra(LocalDateTime.now());
+        reservarEstoqueRequest.setCompraId(notificacao);
+        reservarItemEstoqueProducer.publish(reservarEstoqueRequest);
+        log.info("mensagem enviada {}", notificacao);
     }
 
 }
